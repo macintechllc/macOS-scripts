e1e63712faacac1067f7cc6f6d387156fd254b49
Travis Boothman <46727991+travisboothman@users.noreply.github.com>
2023-02-13 14:53:51 -0800

    Update Does File Exist Template.sh

--- Does File Exist Template.sh
+++ Does File Exist Template.sh
@@ -5,16 +5,20 @@
 ## flag -e for any file regardless of type ## 
 ## flag -f for file ## 
 
-FILE="$4"
-flag="$5"
-FILENAME="$6"
+file=$4
+flag=$5
+shortname="$6"
 
+## Example ##
+## file=/Library/Application\ Support/MyCompany/security.sh ##
+## flag=-f ##
+## shortname="Test Document" ##
 
-if [ $flag "$FILE" ];then
-	echo "$FILENAME exists."
+if [ $flag "$file" ];then
+	echo "$shortname exists."
 	## ENTER ANY COMMANDS YOU WANT RUN HERE IF THE FILE EXISTS ##
 else
-	echo "$FILENAME does not exist"
+	echo "$shortname does not exist"
 	## ENTER ANY COMMANDS YOU WANT RUN HERE IF THE FILE DOES NOT EXIST ##
 fi
 
